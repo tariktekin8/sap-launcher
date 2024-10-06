0fe8e4c73ab44feef6d3cec444fb82d5f4023ca9
tariktekin8 <tariktekin8@gmail.com>
2024-10-06 17:28:30 +0300

    Implemeting messagebox for missing configurations

--- lib/app.py
+++ lib/app.py
@@ -1,11 +1,10 @@
-from tkinter import Tk
+from tkinter import Tk, messagebox
 
 from .providers import SAPLauncher, KeepassProvider, ConfigProvider, Config
 from .screens import LoginScreen, ConfigScreen, LaunchScreen
 
 class App:
 
-    __password: str
     __configProvider: ConfigProvider
     __sapLauncher: SAPLauncher
     __keepassProvider: KeepassProvider
@@ -14,10 +13,20 @@ class App:
         self.__configProvider = ConfigProvider()
         LoginScreen(onLogin=self.onLogin, onConfig=self.onConfig).show()
 
+    # Login screen event
+    def onLogin(self, loginScreen: LoginScreen, password):
+        if not password:
+            return
+        
         config = self.__configProvider.getConfig()
-        self.__keepassProvider = KeepassProvider(
-            config.keepassDbPath, self.__password)
 
+        if not config.sapShortcutExePath or not config.keepassDbPath:
+            messagebox.showwarning(title="Warning", message="Please make sure the configurations are complete!\nSAP shortcut exe path or Keepassxc path are missing!")
+            return
+        
+        loginScreen.close()
+
+        self.__keepassProvider = KeepassProvider(config.keepassDbPath, password)
         self.__sapLauncher = SAPLauncher(config.sapShortcutExePath)
 
         credentials = self.__keepassProvider.getCredentials()
@@ -26,13 +35,7 @@ class App:
         for data in credentials:
             listData.append(data.presentationText)
 
-        LaunchScreen(listData=listData, onFilterChange=self.onFilterChange,
-                     onLaunch=self.onLaunch).show()
-
-    # Login screen event
-    def onLogin(self, loginScreen: LoginScreen, password):
-        self.__password = password
-        loginScreen.close()
+        LaunchScreen(listData=listData, onFilterChange=self.onFilterChange, onLaunch=self.onLaunch).show()
 
     # Login screen event
     def onConfig(self, loginScreen: LoginScreen):
